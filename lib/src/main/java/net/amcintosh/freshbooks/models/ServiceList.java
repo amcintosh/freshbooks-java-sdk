45a493e2d44c01845101706d7bc281adb68d9a79
raernon <bonga@freemail.hu>
2021-10-06 17:36:15 +0200

    Support Services endpoint #5

---
+++ lib/src/main/java/net/amcintosh/freshbooks/models/ServiceList.java
@@ -0,0 +1,29 @@
+package net.amcintosh.freshbooks.models;
+
+import com.google.api.client.util.Key;
+import net.amcintosh.freshbooks.models.api.ServiceListResponse;
+
+import java.util.List;
+
+/**
+ * Results of services list call containing list of services and pagination data.
+ */
+public class ServiceList extends ListResult {
+
+    @Key
+    private final List<Service> services;
+
+    public ServiceList(ServiceListResponse result) {
+        services = result.services;
+        this.pages = new Pages(result.meta.page, result.meta.pages, result.meta.perPage, result.meta.total);
+    }
+
+    /**
+     * Array of services returned by the `list` call.
+     *
+     * @return List of Service objects
+     */
+    public List<Service> getServices() {
+        return services;
+    }
+}
--- lib/src/main/java/net/amcintosh/freshbooks/resources/api/ResourceType.java
+++ lib/src/main/java/net/amcintosh/freshbooks/resources/api/ResourceType.java
@@ -1,7 +1,7 @@
 package net.amcintosh.freshbooks.resources.api;
 
 /**
- * Ued internally to determine the style of FreshBooks API the resource conforms to.
+ * Used internally to determine the style of FreshBooks API the resource conforms to.
  * Resources found under `/accounting` follow a particular style whereas those under
  * `/projects`, `/comments`, and `/timetracking` follow another.
  */
